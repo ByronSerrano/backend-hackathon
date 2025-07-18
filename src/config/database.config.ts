6528053654d24591d0ba519396f28747ba9d602b
Byron Serrano <bsantyserrano@gmail.com>
2025-07-18 12:57:44 -0500

    Added config to database and make it logger and applied global filters http exeption

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,20 +1,21 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
-// import * as fs from 'fs';
-// import * as path from 'path';
+import * as fs from 'fs';
+import * as path from 'path';
 
 dotenv.config();
 
 const configService = new ConfigService();
+const dbSslFile = configService.get<string>('DB_SSL_FILE') || 'ca-certificate.crt';
 
 export const dataBaseConfig: DataSourceOptions = {
   type: 'postgres',
-  host: configService.get<string>('DATABASE_HOST'),
-  port: configService.get<number>('DATABASE_PORT'),
-  username: configService.get<string>('DATABASE_USER'),
-  password: configService.get<string>('DATABASE_PASSWORD'),
-  database: configService.get<string>('DATABASE_NAME'),
+  host: configService.get<string>('DB_HOST'),
+  port: configService.get<number>('DB_PORT'),
+  username: configService.get<string>('DB_USER'),
+  password: configService.get<string>('DB_PASSWORD'),
+  database: configService.get<string>('DB_NAME'),
   entities: [
     __dirname + '/../**/*.entity{.ts,.js}',
     __dirname + '/../**/entity/*.entity{.ts,.js}',
@@ -22,18 +23,18 @@ export const dataBaseConfig: DataSourceOptions = {
   ],
   migrations: [__dirname + '/../migrations/*{.ts,.js}'],
   synchronize: false,
-  // ssl: {
-  //   rejectUnauthorized: true,
-  //   ca: fs
-  //     .readFileSync(
-  //       path.join(
-  //         __dirname,
-  //         '../../',
-  //         configService.get<string>('DATABASE_SSL_FILE'),
-  //       ),
-  //     )
-  //     .toString(),
-  // },
+  ssl: {
+    rejectUnauthorized: false,
+    ca: fs
+      .readFileSync(
+        path.join(
+          __dirname,
+          '../../',
+          dbSslFile,
+        ),
+      )
+      .toString(),
+  },
 };
 
 const dataSource = new DataSource(dataBaseConfig);
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,39 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from '@/src/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ConfigService } from '@nestjs/config';
+import { HttpExceptionFilter } from '@/src/filters/http-exception.filter';
+import { WinstonModule } from 'nest-winston';
+import { winstonConfig } from '@/src/config/logger.config';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    logger: WinstonModule.createLogger(winstonConfig),
+  });
+  const configService = app.get(ConfigService);
+
+  // Cors
+  app.enableCors();
+
+  // Config for swagger
+  if (configService.get('DEVELOPMENT') === 'true') {
+    const config = new DocumentBuilder()
+      .setTitle('IA Parking API')
+      .setDescription('Api for developers')
+      .setVersion('1.0')
+      .addBearerAuth()
+      .build();
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api', app, document, {
+      swaggerOptions: {
+        persistAuthorization: true,
+      },
+    });
+  }
+
+  // Apply global exception filter
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
